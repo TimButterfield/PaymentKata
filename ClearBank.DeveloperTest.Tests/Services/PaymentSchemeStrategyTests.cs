fcd1eba92970a448fc51f422a894cf7a12d902cb
Tim Butterfield <tim@timbutterfield.co.uk>
2022-10-13 12:06:11 +0100

    Introduced interfaces and using DI, with tests

---
+++ ClearBank.DeveloperTest.Tests/Services/PaymentSchemeStrategyTests.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ClearBank.DeveloperTest.Services;
+using ClearBank.DeveloperTest.Types;
+using FluentAssertions;
+using Xunit;
+
+namespace ClearBank.DeveloperTest.Tests.Services;
+
+public class PaymentSchemeStrategyTests
+{
+    [Theory]
+    [InlineData(PaymentScheme.Bacs, typeof(BacsPaymentStrategy))]
+    [InlineData(PaymentScheme.FasterPayments, typeof(FasterPaymentsPaymentStrategy))]
+    [InlineData(PaymentScheme.Chaps, typeof(ChapsPaymentsPaymentStrategy))]
+    public void WhenSelectingTheRightStrategy_ThenTheRightStrategyIsSelected(PaymentScheme paymentScheme, Type type)
+    {
+        var paymentSchemeStrategy = new List<IPaymentStrategy>
+        {
+            new BacsPaymentStrategy(), 
+            new FasterPaymentsPaymentStrategy(), 
+            new ChapsPaymentsPaymentStrategy()
+        };
+
+        var paymentRequest = new MakePaymentRequest { PaymentScheme = paymentScheme };
+
+        var selectedPaymentStrategy = paymentSchemeStrategy.FirstOrDefault(x => x.Applies(paymentRequest));
+
+        selectedPaymentStrategy.Should().NotBeNull();
+        selectedPaymentStrategy.Should().BeOfType(type);
+
+    }
+}
--- ClearBank.DeveloperTest.Tests/Services/PaymentServiceTests.cs
+++ ClearBank.DeveloperTest.Tests/Services/PaymentServiceTests.cs
@@ -1,39 +1,96 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
+using ClearBank.DeveloperTest.Data;
 using ClearBank.DeveloperTest.Services;
 using ClearBank.DeveloperTest.Types;
 using FluentAssertions;
+using NSubstitute;
 using Xunit;
 
 namespace ClearBank.DeveloperTest.Tests.Services;
 
 public class PaymentServiceTests
 {
-    //placeholder
-}
-
-public class PaymentSchemeStrategyTests
-{
-    [Theory]
-    [InlineData(PaymentScheme.Bacs, typeof(BacsPaymentStrategy))]
-    [InlineData(PaymentScheme.FasterPayments, typeof(FasterPaymentsPaymentStrategy))]
-    [InlineData(PaymentScheme.Chaps, typeof(ChapsPaymentsPaymentStrategy))]
-    public void WhenSelectingTheRightStrategy_ThenTheRightStrategyIsSelected(PaymentScheme paymentScheme, Type type)
+    [Fact]
+    private void WhenRequestIsNotValid_ThenAccountIsNotUpdated()
     {
-         var paymentSchemeStrategy = new List<IPaymentStrategy>
-         {
-             new BacsPaymentStrategy(), 
-             new FasterPaymentsPaymentStrategy(), 
-             new ChapsPaymentsPaymentStrategy()
-         };
+        //arrange
+        const string debtorAccountNumber = "123456789";
+        var dataStore = Substitute.For<IDataStore>();
+        var paymentStrategy = Substitute.For<IPaymentStrategy>();
+        paymentStrategy.Applies(Arg.Any<MakePaymentRequest>()).Returns(true); 
+        paymentStrategy.ValidateRequest(Arg.Any<MakePaymentRequest>(), Arg.Any<Account>()).Returns(new MakePaymentResult{Success = false}); 
+            
+        var sut = new PaymentService(dataStore, GetPaymentStrategies(paymentStrategy));
+        
+        var paymentRequest = new MakePaymentRequest { PaymentScheme = PaymentScheme.Bacs, DebtorAccountNumber =  debtorAccountNumber};
+        
+        //act
+        var result = sut.MakePayment(paymentRequest);
 
-         var paymentRequest = new MakePaymentRequest { PaymentScheme = paymentScheme };
+        //assert
+        dataStore.Received(1).GetAccount(debtorAccountNumber);
+        dataStore.DidNotReceive().UpdateAccount(Arg.Any<Account>());
+        result.Success.Should().BeFalse();
+    }
+    
+    [Fact]
+    private void WhenMakingAValidPayment_AndAccountFound()
+    {
+        //arrange
+        const string debtorAccountNumber = "123456789";
+        const int balance = 1000;
+        
+        var paymentAmount = balance / 2;
+        var dataStore = Substitute.For<IDataStore>();
+        var paymentStrategy = Substitute.For<IPaymentStrategy>();
+        dataStore.GetAccount(Arg.Any<string>()).Returns(new Account{AllowedPaymentSchemes = AllowedPaymentSchemes.Bacs, Balance = balance});
+        paymentStrategy.Applies(Arg.Any<MakePaymentRequest>()).Returns(true); 
+        paymentStrategy.ValidateRequest(Arg.Any<MakePaymentRequest>(), Arg.Any<Account>()).Returns(new MakePaymentResult{Success = true}); 
+        
+        var sut = new PaymentService(dataStore, GetPaymentStrategies(paymentStrategy));
+        var paymentRequest = new MakePaymentRequest { PaymentScheme = PaymentScheme.Bacs, DebtorAccountNumber =  debtorAccountNumber, Amount = paymentAmount};
+        
+        //act
+        var result = sut.MakePayment(paymentRequest);
 
-         var selectedPaymentStrategy = paymentSchemeStrategy.FirstOrDefault(x => x.Applies(paymentRequest));
+        //assert
+        dataStore.Received(1).GetAccount(debtorAccountNumber);
+        dataStore.Received().UpdateAccount(Arg.Is<Account>(x => x.Balance == balance - paymentAmount));
+        result.Success.Should().BeTrue();
+    }
+    
+    //This test demonstrates a bug in the code, caused by default sucess state of true. 
+    //Need to see it's desired behaviour before removing!
+    [Fact]
+    private void WhenPaymentSchemeIsNotInRange_ThenResultIsTrue()
+    {
+        const string debtorAccountNumber = "123456789";
+        const int balance = 1000;
+        
+        var paymentAmount = balance / 2;
+        var invalidPaymentScheme = (PaymentScheme)10;
+        var dataStore = Substitute.For<IDataStore>();
+       
+        dataStore.GetAccount(Arg.Any<string>()).Returns(new Account{AllowedPaymentSchemes = AllowedPaymentSchemes.Bacs, Balance = balance});
+        var paymentStrategy = Substitute.For<IPaymentStrategy>();
+        paymentStrategy.Applies(Arg.Any<MakePaymentRequest>()).Returns(false);
+        
+        var sut = new PaymentService(dataStore, GetPaymentStrategies(paymentStrategy));
+        var paymentRequest = new MakePaymentRequest { PaymentScheme = invalidPaymentScheme, DebtorAccountNumber =  debtorAccountNumber, Amount = paymentAmount};
+        
+        //act
+        var result = sut.MakePayment(paymentRequest);
 
-         selectedPaymentStrategy.Should().NotBeNull();
-         selectedPaymentStrategy.Should().BeOfType(type);
+        dataStore.Received(1).GetAccount(debtorAccountNumber);
+        dataStore.Received().UpdateAccount(Arg.Is<Account>(x => x.Balance == balance - paymentAmount));
+        result.Success.Should().BeTrue();
+    }
 
+    private IEnumerable<IPaymentStrategy> GetPaymentStrategies(IPaymentStrategy paymentStrategy)
+    {
+        return new List<IPaymentStrategy>
+        {
+            paymentStrategy
+        };
     }
 }
